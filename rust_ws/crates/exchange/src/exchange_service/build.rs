34c46732202fc40dbd6bd4069b3f9d4d33bdca5b
dzungngnbh <yearly.woven0j@icloud.com>
2025-04-03 03:17:29 +0700

    feat(modal): add modal

--- rust_ws/crates/exchange/src/exchange_service/build.rs
+++ rust_ws/crates/exchange/src/exchange_service/build.rs
@@ -1,5 +1,3 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    capnpc::CompilerCommand::new().file("pubsub.capnp").run()?;
-    capnpc::CompilerCommand::new().file("pubsub2.capnp").run()?;
     Ok(())
 }
